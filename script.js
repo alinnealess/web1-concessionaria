b7ce74e4fdd928c2ee7cb10e3f3f0c3604369754
Alinne Alessandra <alinne.alessandra@gmail.com>
2024-06-20 20:01:40 -0300

    Update styles.css and menus.html

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-//pagina de login
+// Definindo classes Veiculo e Carro
+class Veiculo {
+  constructor(marca, modelo, anoFabricacao, cor, tipo, quilometragem, numeroPortas, preco, imagem) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.anoFabricacao = anoFabricacao;
+    this.cor = cor;
+    this.tipo = tipo;
+    this.quilometragem = quilometragem;
+    this.numeroPortas = numeroPortas;
+    this.preco = preco;
+    this.imagem = imagem;
+  }
+}
 
+class Carro extends Veiculo {
+  constructor(marca, modelo, anoFabricacao, cor, tipo, quilometragem, numeroPortas, preco, imagem) {
+    super(marca, modelo, anoFabricacao, cor, tipo, quilometragem, numeroPortas, preco, imagem);
+  }
+}
+
+// Função de login
 document.getElementById('loginForm').addEventListener('submit', function(event) {
   event.preventDefault();
   const username = document.getElementById('username').value;
@@ -12,20 +32,13 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
   }
 });
 
-// Script para a página de login
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
+// Função de logout
+function logout() {
+  alert('Você foi deslogado com sucesso!');
+  window.location.href = 'index.html';
+}
 
-  if (username === 'admin' && password === 'admin') {
-    window.location.href = 'menus.html';
-  } else {
-    alert('Nome de usuário ou senha incorretos!');
-  }
-});
-
-// Script para cadastrar veículos
+// Função para cadastrar veículos
 function cadastrarVeiculo() {
   const marca = document.getElementById('marca').value;
   const modelo = document.getElementById('modelo').value;
@@ -35,60 +48,64 @@ function cadastrarVeiculo() {
   const quilometragem = document.getElementById('km').value;
   const numeroPortas = document.querySelector('input[name="numeroPortas"]:checked').value;
   const preco = document.getElementById('preco').value;
-  const imagem = document.getElementById('exampleFormControlFile1').files[0] ? document.getElementById('exampleFormControlFile1').files[0].name : 'car_placeholder.jpg';
+  const imagemInput = document.getElementById('exampleFormControlFile1').files[0];
 
-  if (!marca || !modelo || !anoFabricacao || !cor || !tipo || !quilometragem || !numeroPortas || !preco) {
-    alert('Por favor, preencha todos os campos obrigatórios.');
+  if (!marca || !modelo || !anoFabricacao || !cor || !tipo || !quilometragem || !numeroPortas || !preco || !imagemInput) {
+    alert('Por favor, preencha todos os campos obrigatórios, incluindo a imagem.');
     return;
   }
 
-  const veiculo = {
-    marca,
-    modelo,
-    anoFabricacao,
-    cor,
-    tipo,
-    quilometragem,
-    numeroPortas,
-    preco,
-    imagem
-  };
+  const imagem = imagemInput.name;
+  const carro = new Carro(marca, modelo, anoFabricacao, cor, tipo, quilometragem, numeroPortas, preco, imagem);
 
   let veiculos = JSON.parse(localStorage.getItem('veiculos')) || [];
-  veiculos.push(veiculo);
+  veiculos.push(carro);
   localStorage.setItem('veiculos', JSON.stringify(veiculos));
 
   alert('Veículo cadastrado com sucesso!');
   window.location.href = 'listar_buscar_veiculos.html';
 }
 
-// Script para listar veículos
+
+// Função para listar veículos
 document.addEventListener('DOMContentLoaded', function() {
-  carregarVeiculos();
+  if (window.location.pathname.includes('listar_buscar_veiculos.html')) {
+    exibirTodos();
+  } else if (window.location.pathname.includes('excluir.html')) {
+    exibirTodosExcluir();
+  }
 });
 
-function carregarVeiculos() {
+function exibirTodos() {
   const veiculos = JSON.parse(localStorage.getItem('veiculos')) || [];
   const veiculosLista = document.getElementById('veiculosLista');
   veiculosLista.innerHTML = '';
 
-  veiculos.forEach(veiculo => {
+  let row;
+  veiculos.forEach((veiculo, index) => {
+    if (index % 3 === 0) {
+      row = document.createElement('div');
+      row.className = 'row mb-4';
+      veiculosLista.appendChild(row);
+    }
+
     const veiculoItem = document.createElement('div');
-    veiculoItem.className = 'card';
+    veiculoItem.className = 'col-md-4';
     veiculoItem.innerHTML = `
-      <img class="card-img-top" src="resources/${veiculo.imagem}" alt="Imagem de capa do card">
-      <div class="card-body">
-        <h5 class="card-title">${veiculo.marca} ${veiculo.modelo}</h5>
-        <p class="card-text">Ano: ${veiculo.anoFabricacao}</p>
-        <p class="card-text">Cor: ${veiculo.cor}</p>
-        <p class="card-text">Tipo: ${veiculo.tipo}</p>
-        <p class="card-text">Quilometragem: ${veiculo.quilometragem} km</p>
-        <p class="card-text">Número de Portas: ${veiculo.numeroPortas}</p>
-        <p class="card-text">Preço: R$ ${veiculo.preco}</p>
-        <p class="card-text"><small class="text-muted">Atualizados agora</small></p>
+      <div class="card">
+        <img class="card-img-top" src="resources/${veiculo.imagem}" alt="Imagem de capa do card">
+        <div class="card-body">
+          <h5 class="card-title">${veiculo.marca} ${veiculo.modelo}</h5>
+          <p class="card-text">Ano: ${veiculo.anoFabricacao}</p>
+          <p class="card-text">Cor: ${veiculo.cor}</p>
+          <p class="card-text">Tipo: ${veiculo.tipo}</p>
+          <p class="card-text">Quilometragem: ${veiculo.quilometragem} km</p>
+          <p class="card-text">Número de Portas: ${veiculo.numeroPortas}</p>
+          <p class="card-text">Preço: R$ ${veiculo.preco}</p>
+        </div>
       </div>
     `;
-    veiculosLista.appendChild(veiculoItem);
+    row.appendChild(veiculoItem);
   });
 }
 
@@ -103,7 +120,9 @@ function filtrarVeiculos() {
   const veiculosLista = document.getElementById('veiculosLista');
   veiculosLista.innerHTML = '';
 
-  veiculos.forEach(veiculo => {
+  let row;
+  let encontrouVeiculo = false;
+  veiculos.forEach((veiculo, index) => {
     if (
       (marcaFiltro === '' || veiculo.marca.toLowerCase().includes(marcaFiltro)) &&
       (modeloFiltro === '' || veiculo.modelo.toLowerCase().includes(modeloFiltro)) &&
@@ -111,56 +130,74 @@ function filtrarVeiculos() {
       (corFiltro === '' || veiculo.cor.toLowerCase().includes(corFiltro)) &&
       (tipoFiltro === '' || veiculo.tipo.toLowerCase().includes(tipoFiltro))
     ) {
+      if (index % 3 === 0) {
+        row = document.createElement('div');
+        row.className = 'row mb-4';
+        veiculosLista.appendChild(row);
+      }
+
       const veiculoItem = document.createElement('div');
-      veiculoItem.className = 'card';
+      veiculoItem.className = 'col-md-4';
       veiculoItem.innerHTML = `
-        <img class="card-img-top" src="resources/${veiculo.imagem}" alt="Imagem de capa do card">
-        <div class="card-body">
-          <h5 class="card-title">${veiculo.marca} ${veiculo.modelo}</h5>
-          <p class="card-text">Ano: ${veiculo.anoFabricacao}</p>
-          <p class="card-text">Cor: ${veiculo.cor}</p>
-          <p class="card-text">Tipo: ${veiculo.tipo}</p>
-          <p class="card-text">Quilometragem: ${veiculo.quilometragem} km</p>
-          <p class="card-text">Número de Portas: ${veiculo.numeroPortas}</p>
-          <p class="card-text">Preço: R$ ${veiculo.preco}</p>
-          <p class="card-text"><small class="text-muted">Atualizados agora</small></p>
+        <div class="card">
+          <img class="card-img-top" src="resources/${veiculo.imagem}" alt="Imagem de capa do card">
+          <div class="card-body">
+            <h5 class="card-title">${veiculo.marca} ${veiculo.modelo}</h5>
+            <p class="card-text">Ano: ${veiculo.anoFabricacao}</p>
+            <p class="card-text">Cor: ${veiculo.cor}</p>
+            <p class="card-text">Tipo: ${veiculo.tipo}</p>
+            <p class="card-text">Quilometragem: ${veiculo.quilometragem} km</p>
+            <p class="card-text">Número de Portas: ${veiculo.numeroPortas}</p>
+            <p class="card-text">Preço: R$ ${veiculo.preco}</p>
+          </div>
         </div>
       `;
-      veiculosLista.appendChild(veiculoItem);
+      row.appendChild(veiculoItem);
+      encontrouVeiculo = true;
     }
   });
+
+  if (!encontrouVeiculo) {
+    alert('Não há veículos correspondentes ao filtro.');
+  }
 }
 
-
-// Carregar veículos na página de exclusão
-document.addEventListener('DOMContentLoaded', function() {
-  if (window.location.pathname.includes('excluir_veiculos.html')) {
-    carregarVeiculosParaExcluir();
-  }
-});
-
-function carregarVeiculosParaExcluir() {
+function exibirTodosExcluir() {
   const veiculos = JSON.parse(localStorage.getItem('veiculos')) || [];
   const veiculosLista = document.getElementById('veiculosLista');
   veiculosLista.innerHTML = '';
 
+  let row;
   veiculos.forEach((veiculo, index) => {
+    if (index % 3 === 0) {
+      row = document.createElement('div');
+      row.className = 'row mb-4';
+      veiculosLista.appendChild(row);
+    }
+
     const veiculoItem = document.createElement('div');
-    veiculoItem.className = 'list-group-item';
+    veiculoItem.className = 'col-md-4';
     veiculoItem.innerHTML = `
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="${index}" id="veiculo${index}">
-        <label class="form-check-label" for="veiculo${index}">
-          <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1">${veiculo.marca} ${veiculo.modelo}</h5>
-            <small>Ano: ${veiculo.anoFabricacao}</small>
+      <div class="card">
+        <div class="form-check">
+          <img class="card-img-top" src="resources/${veiculo.imagem}" alt="Imagem de capa do card">
+          <div class="card-body">
+            <h5 class="card-title">${veiculo.marca} ${veiculo.modelo}</h5>
+            <p class="card-text">Ano: ${veiculo.anoFabricacao}</p>
+            <p class="card-text">Cor: ${veiculo.cor}</p>
+            <p class="card-text">Tipo: ${veiculo.tipo}</p>
+            <p class="card-text">Quilometragem: ${veiculo.quilometragem} km</p>
+            <p class="card-text">Número de Portas: ${veiculo.numeroPortas}</p>
+            <p class="card-text">Preço: R$ ${veiculo.preco}</p>
+            <div class="d-flex align-items-center mt-3">
+              <input class="form-check-input" type="checkbox" value="${index}" id="veiculo${index}">
+              <label class="form-check-label ml-2" for="veiculo${index}">Excluir este item</label>
+            </div>
           </div>
-          <p class="mb-1">Cor: ${veiculo.cor}, Tipo: ${veiculo.tipo}</p>
-          <small>Quilometragem: ${veiculo.quilometragem} km, Preço: R$ ${veiculo.preco}</small>
-        </label>
+        </div>
       </div>
     `;
-    veiculosLista.appendChild(veiculoItem);
+    row.appendChild(veiculoItem);
   });
 }
 
@@ -179,16 +216,5 @@ function excluirVeiculos() {
 
   localStorage.setItem('veiculos', JSON.stringify(veiculos));
   alert('Veículo(s) excluído(s) com sucesso!');
-  carregarVeiculosParaExcluir();
-}
-
-
-// Função de logout
-function logout() {
-  // Limpa qualquer informação de sessão (se necessário)
-  localStorage.clear(); // Descomente esta linha se desejar limpar o localStorage ao deslogar
-  alert('Você foi deslogado com sucesso!')
-
-  // Redireciona para a página de login
-  window.location.href = 'index.html';
+  exibirTodosExcluir();
 }
